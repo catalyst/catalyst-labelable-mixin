aafb475a8637acc8869b7469d40c0789e2998bc0
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-02-15 14:32:36 +1300

    removed unused file.

--- custom_typings/sortPackageJson.d.ts
+++ custom_typings/sortPackageJson.d.ts
@@ -1,4 +1,4 @@
 declare module 'sort-package-json' {
   export = sortPackage;
-  declare function sortPackage(packageJson: object): object;
+  declare function sortPackage(packageJson: object, options: any): object;
 }
--- scripts/types/sort-package-json.d.ts
+++
@@ -1,7 +0,0 @@
-export = index;
-declare function index(packageJson: any, options: any): any;
-declare namespace index {
-  const sortOrder: string[];
-  // Circular reference from index
-  const sortPackageJson: any;
-}
